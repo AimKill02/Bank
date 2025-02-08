442a4ac06548d264f6bf1274d4ed7b3a407805d2
AimKill02 <Kevin.Saerang@binus.ac.id>
2025-02-08 23:29:09 +0700

    Mostly just Fix

--- src/Bank_Account.java
+++ src/Bank_Account.java
@@ -4,7 +4,6 @@ public class Bank_Account {
     protected final String Card_Number;
     protected final int CVC;
     protected double Balance;
-
     // Constructor
     public Bank_Account (String AccountName, String Identification_Number, int CVC, double Initial_Balance) {
         this.Name = AccountName;
@@ -12,7 +11,6 @@ public class Bank_Account {
         this.CVC = CVC;
         this.Balance = Initial_Balance;
     }
-
     // Deposit
     public void deposit_Money (double Amount) {
         if (Amount > 0) {
@@ -22,7 +20,6 @@ public class Bank_Account {
             System.out.println("Invalid Amount\n");
         }
     }
-
     // Withdraw
     public void Withdraw_Money (double Amount) {
         if (Amount > 0 && Amount <= Balance) {
@@ -32,12 +29,10 @@ public class Bank_Account {
             System.out.println("Invalid Amount or Insufficent Balance\n");
         }
     }
-
     // Check Info
     public void CheckInfo() {
         System.out.println("\nCardholder Name: " + Name + "\nCard Number: " + Card_Number + "\nCVC: " + CVC + "\nCurrent Balance: $" + Balance);
     }
-
     // Test Run
     public static void main(String[] args) {
         Bank_Account user = new Bank_Account("Jane Doe", "5550 1309 6672 6224", 324, 12000);
@@ -51,24 +46,20 @@ public class Bank_Account {
 class Savings_Account extends Bank_Account{
     //Initials
     private final double Interest;
-
     // Constructor
     public Savings_Account (String AccountName, String Identification_Number, int CVC, double Initial_Balance, double Interest_Rate){
         super(AccountName, Identification_Number, CVC, Initial_Balance);
         this.Interest = Interest_Rate;
     }
-
     // Applying Interest
     public void Apply_Interest (){
         Balance += Balance * (Interest / 100);
         System.out.println("\nInterest Applied.\nNew Balance: $" + Balance);
     }
-
     // Check Info
     @Override public void CheckInfo() {
         System.out.println("\nCardholder Name: " + Name + "\nCard Number: " + Card_Number + "\nCVC: " + CVC + "\nCurrent Balance: $" + Balance + "\nInterest: " + Interest + "%");
     }
-
     // Test Run
     public static void main(String[] args) {
         Savings_Account user = new Savings_Account("Jane Doe", "5550 1309 6672 6224", 324, 12000, 5);
